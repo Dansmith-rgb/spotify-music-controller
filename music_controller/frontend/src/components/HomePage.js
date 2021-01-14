b9e9e5bc00aa0adb052e62125f03e2fa0758f9b7
Dan <68809853+Dansmith-rgb@users.noreply.github.com>
2021-01-14 16:14:55 +0000

    Add files via upload

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage.js";
+import { ButtonGroup, Button, Grid, Typography } from "@material-ui/core";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect,} from "react-router-dom";
+import Room from "./Room";
+
+export default class HomePage extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            roomCode: null,
+        }
+        this.clearRoomCode = this.clearRoomCode.bind(this);
+    }
+
+    async componentDidMount() {
+        fetch('/api/user-in-room')
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                roomCode: data.code,
+            });
+        });
+    }
+
+    renderHomePage() {
+        return (
+            <Grid conatiner spacing={3} align="center">
+                <Grid item xs={12}>
+                    <Typography variant="h3" component="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to='/join' component={ Link }>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to='/create' component={ Link }>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
+    }
+
+    clearRoomCode() {
+        this.setState({
+            roomCode: null,
+        });
+    }
+
+    render() {
+        return (<Router>
+            <Switch>
+                <Route exact path='/' render={() => {
+                    return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`} ></Redirect>) : this.renderHomePage()
+                }}></Route>
+                <Route path='/join' component={RoomJoinPage}></Route>
+                <Route path='/create' component={CreateRoomPage}></Route>
+                <Route path='/room/:roomCode' render={(props) => {
+                    return <Room {...props} leaveRoomCallback={this.clearRoomCode}></Room>;
+                }}></Route>
+            </Switch>
+        </Router>);
+    };
+}
---
+++ music_controller/frontend/src/components/Room.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+export default class Room extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            votesToSkip: 2,
+            guestCanPause: false,
+            isHost: false,
+        };
+        this.roomCode = this.props.match.params.roomCode;
+        this.getRoomDetails();
+        this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+    }
+
+    getRoomDetails() {
+        fetch('/api/get-room' + '?code=' + this.roomCode).then((response) => { 
+            if (!response.ok) {
+                this.props.leaveRoomCallback();
+                this.props.history.push("/");
+            }
+            return response.json();
+        }).then((data) => {
+            this.setState({
+                votesToSkip: data.votes_to_skip,
+                guestCanPause: data.guest_can_pause,
+                isHost: data.is_host,
+            });
+        });
+    }
+
+    leaveButtonPressed() {
+        const requestOptions = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+        };
+
+        fetch("/api/leave-room", requestOptions).then((_response) => {
+            this.props.leaveRoomCallback();
+            this.props.history.push('/');
+        });
+        
+    }
+
+    render() {
+        return ( 
+        <Grid container spacing={1} align="center">
+            <Grid item xs={12}>
+                <Typography variant="h4" component="h4">
+                    Room Code: {this.roomCode}
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h6" component="h6">
+                Votes: {this.state.votesToSkip}
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h6" component="h6">
+                Guest Can pause: {this.state.guestCanPause.toString()}
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h6" component="h6">
+                    Host: {this.state.isHost.toString()}
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+            <Button color="secondary" variant="contained" onClick={this.leaveButtonPressed}>Leave Room</Button>
+            </Grid>
+        
+        
+        </Grid>
+        );
+    }
+}
+
